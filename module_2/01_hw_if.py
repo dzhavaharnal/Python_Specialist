f91f40c9782a21cfa6ffd37be6a5cc201fe58844
dzhavaharnal <112697031+dzhavaharnal@users.noreply.github.com>
2022-10-04 12:27:05 +0300

    Delete module_2 directory

--- module_2/01_hw_if.py
+++
@@ -1,8 +0,0 @@
-n = int(input("n "))
-m = int(input("m "))
-k = int(input("k "))
-
-if (k % n == 0 or k % m == 0) and (k >= min(m,n)):
-    print("yes")
-else:
-      print("no")
--- module_2/04_hw_if.py
+++
@@ -1,9 +0,0 @@
-n = int(input("n: "))
-
-if n % 3 ==0 or n % 5 == 0:
-    print ("Да")
-elif (n % 5) % 3 == 0 or (n % 3) % 5 == 0:
-    print ("Да")
-else:
-    print("Нет")
-    
